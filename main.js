9bc90fe4c1c8643591821182faabd26387c26212
kujotarooo.o@gmail.com <kujotarooo.o@gmail.com>
2023-02-24 20:57:10 +0800

    second commit

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("gif").style.visibility="hidden"
+document.getElementById("c").style.visibility="hidden"
+function aa(){
+  document.getElementById("gif").style.visibility="visible"
+  document.getElementById("c").style.visibility="hidden"
+  document.getElementById("b").style.visibility="hidden"
+  document.getElementById("a").style.visibility="hidden"
+  document.getElementById("aaa").style.visibility="hidden"
+}
+function bb() {
+  document.getElementById("b").style.visibility="hidden"
+  document.getElementById("c").style.visibility="visible"
+}
+function cc() {
+  document.getElementById("b").style.visibility="visible"
+  document.getElementById("c").style.visibility="hidden"
+}
